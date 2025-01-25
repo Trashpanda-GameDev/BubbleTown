f20f6722e099429dcc2ae90b01ce5a4616d70f6e
André Santiago <andre.mpc.santiago@gmail.com>
2025-01-25 23:57:15 +0100

    fix(scripts.gameplay.thoughtresource): fix improper initialization

--- scripts/gameplay/ThoughtResource.gd
+++ scripts/gameplay/ThoughtResource.gd
@@ -2,10 +2,6 @@ class_name ThoughtResource extends Resource
 
 @export var texture: Texture2D
 
-func _init(texture: Texture2D) -> void:
-	self.id = ResourceLoader.get_resource_uid(resource_path)
-	self.texture = texture
-
 func get_sprite() -> Sprite2D:
 	var sprite: Sprite2D = Sprite2D.new()
 	sprite.texture = self.texture
