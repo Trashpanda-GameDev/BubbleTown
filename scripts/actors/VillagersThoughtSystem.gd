eb20f92e288fb887ac76a827d58d870c8e4a81e1
André Santiago <andre.mpc.santiago@gmail.com>
2025-01-26 00:02:26 +0100

    fix(scripts.actors.VillagersThoughtSystem): remove logging

--- scripts/actors/VillagersThoughtSystem.gd
+++ scripts/actors/VillagersThoughtSystem.gd
@@ -6,11 +6,8 @@ class_name VillagersThoughtSystem extends Node
 
 func _ready() -> void:
 	var thought_resource_ids := thoughts_repository.get_thought_resource_ids()
-	print(thought_resource_ids)
-	print(thoughts_repository)
 
 	for villager in villagers_system.villagers:
 		var thought_resource_id := thought_resource_ids[randi_range(0, len(thought_resource_ids) - 1)]
 		var thought := thoughts_repository.get_thought_by_resource_id(thought_resource_id)
-		print(thought)
 		villager.thought.set_thought(thought)
