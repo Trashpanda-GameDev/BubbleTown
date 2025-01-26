dd9faee2f563beff1f3f298c397df99253eb89c1
André Santiago <andre.mpc.santiago@gmail.com>
2025-01-26 06:42:21 +0100

    feat(thoughts): add collecting thoughts from villagers

--- scripts/actors/PlayerController.gd
+++ scripts/actors/PlayerController.gd
@@ -1,6 +1,10 @@
 class_name PlayerController extends Node
 
+@export var thoughtsRepository: ThoughtsRepository
+
 @export var movement: PlayerMovement
+
+var inventory: ThoughtsInventory
 
 var _current_villager: VillagerSystem
 var _is_near_villager: bool = false
@@ -8,8 +12,24 @@ var _is_near_villager: bool = false
 signal show_post_photo_panel
 signal toggle_prompt_message
 
+func _init() -> void:
+	inventory = ThoughtsInventory.new(thoughtsRepository)
+
+func _process(_delta: float) -> void:
+	self._handle_villager_interaction()
+
 func _on_player_house_toggle_end_day_prompt(show_prompt: bool) -> void:
 	toggle_prompt_message.emit(show_prompt)
+
+func _handle_villager_interaction() -> void:
+	if !_is_near_villager || !Input.is_action_just_pressed("interact"):
+		return
+	if !_current_villager.thought.has_thought():
+		return
+
+	var thought := _current_villager.thought.get_thought()
+	_current_villager.thought.clear_thought()
+	self.inventory.add_thought(thought)
 
 func _on_villager_nearby(body: Node2D) -> void:
 	var parent: Node = body.get_parent()
--- scripts/actors/VillagerSystem.gd
+++ scripts/actors/VillagerSystem.gd
@@ -5,7 +5,11 @@ class_name VillagerSystem extends Node
 @export_group("UI")
 @export var thoughtView: VillagerThoughtView
 
-func _show_thought_on_player_collision_start(body: Node2D) -> void:
+func _ready() -> void:
+	thought.on_thought_changed.connect(_handle_thought_change)
+	thought.on_thought_cleared.connect(_handle_thought_clear)
+
+func _show_thought_on_player_collision_start(_body: Node2D) -> void:
 	var current_thought: Thought = thought.get_thought()
 	if current_thought == null:
 		return
@@ -13,5 +17,14 @@ func _show_thought_on_player_collision_start(body: Node2D) -> void:
 	thoughtView.texture.texture = current_thought.get_texture()
 	thoughtView.visible = true
 
-func _hide_thought_on_player_collision_start(body: Node2D) -> void:
+func _hide_thought_on_player_collision_start(_body: Node2D) -> void:
+	thoughtView.texture.texture = null
+	thoughtView.visible = false
+
+func _handle_thought_change(thought: Thought) -> void:
+	if thoughtView.visible:
+		thoughtView.texture.texture = thought.get_texture()
+
+func _handle_thought_clear() -> void:
+	thoughtView.texture.texture = null
 	thoughtView.visible = false
